5f02e72de34f82b01e8ee77033dae65a48b09513
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2024-05-01 10:44:03 +0800

    feat: authentication for removing activity

--- src/routers/activities/remove.rs
+++ src/routers/activities/remove.rs
@@ -1,7 +1,6 @@
-use crate::models::{
-    activities::Activity,
-    response::{ErrorResponse, ResponseStatus, SuccessResponse},
-};
+use crate::{models::{
+    activities::Activity, groups::GroupPermission, response::{ErrorResponse, ResponseStatus, SuccessResponse}
+}, utils::jwt::UserData};
 use axum::{
     extract::{Extension, Path},
     http::StatusCode,
@@ -14,6 +13,7 @@ use tokio::sync::Mutex;
 
 pub async fn remove_activity(
     Extension(db): Extension<Arc<Mutex<Database>>>,
+    user: UserData,
     Path(id): Path<String>,
 ) -> impl IntoResponse {
     let db = db.lock().await;
@@ -29,6 +29,40 @@ pub async fn remove_activity(
         return (StatusCode::BAD_REQUEST, Json(response));
     }
     let id = id.unwrap();
+    if user.perms.contains(&GroupPermission::Admin) || user.perms.contains(&GroupPermission::Department) {
+    } else {
+        let activity = collection.find_one(doc! {"_id": id}, None).await;
+        if let Err(e) = activity {
+            let response = ErrorResponse {
+                status: ResponseStatus::Error,
+                code: 500,
+                message: "Failed to find activity: ".to_string() + &e.to_string(),
+            };
+            let response = serde_json::to_string(&response).unwrap();
+            return (StatusCode::INTERNAL_SERVER_ERROR, Json(response));
+        }
+        let activity = activity.unwrap();
+        if let None = activity {
+            let response = ErrorResponse {
+                status: ResponseStatus::Error,
+                code: 404,
+                message: "Activity not found".to_string(),
+            };
+            let response = serde_json::to_string(&response).unwrap();
+            return (StatusCode::NOT_FOUND, Json(response));
+        }
+        let activity = activity.unwrap();
+        let creator = activity.creator;
+        if creator != user.id {
+            let response = ErrorResponse {
+                status: ResponseStatus::Error,
+                code: 403,
+                message: "Permission denied".to_string(),
+            };
+            let response = serde_json::to_string(&response).unwrap();
+            return (StatusCode::FORBIDDEN, Json(response));
+        }
+    }
     let filter = doc! {"_id": id};
     let result = collection.delete_one(filter, None).await;
     if let Err(e) = result {
