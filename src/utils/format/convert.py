0428c451ae796668d8250351f50cd4ae783e25a8
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2024-05-03 15:00:26 +0800

    feat: improve python code

--- src/utils/format/convert.py
+++ src/utils/format/convert.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
-def read_and_save_csv(input_path, output_path):
-    df = pd.read_csv(input_path, encoding='utf-8')
-    df.to_excel(output_path, index=False)
+def to_excel(input_path: str, output_path: str):
+    import pandas as pd
+    pd.read_csv(input_path, encoding='utf-8').to_excel(output_path, index=False)
